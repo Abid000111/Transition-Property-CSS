9de4ecd8b903b96475c104a422db100d8aceb422
Abid Shahrier <abidofficial0001@gmail.com>
2024-01-21 22:52:26 +0600

    Updated all files

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let box = document.querySelector(".box");
 
 let turn = 0;
 box.addEventListener("click", () => {
-    if (turn === 0) {
-        turn = 1;
-        box.style.backgroundColor = "blue";
-        box.classList.add("translate");
-    } else {
-        turn = 0;
-        box.style.backgroundColor = "aquamarine";
-        box.classList.remove("translate");
-    }
-})
+	if (turn === 0) {
+		turn = 1;
+		box.style.backgroundColor = "blue";
+		box.classList.add("translate");
+	} else {
+		turn = 0;
+		box.style.backgroundColor = "aquamarine";
+		box.classList.remove("translate");
+	}
+});
